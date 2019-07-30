f9b14a13f9b416aa92246772ed604288fcd386f9
pmamueng <pmamueng@gmail.com>
2019-07-29 20:17:58 -0500

    is_pangram code has been created

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,12 @@
 def is_pangram(sentence):
-    pass
+    input = sorted(set(sentence.lower())) #sorted is optional
+    count = 0
+
+    for ch in input:
+        if ord(ch) in range(ord('a'),ord('z')+1):
+            count+=1
+
+    if count == 26:
+        return True
+    else:
+        return False
