24f2d173b0f3ae69bdde52bc99a2f8d285dde954
pmamueng <pmamueng@gmail.com>
2019-07-29 20:06:23 -0500

    Change two_fer.py to be more readable

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,2 @@
-def two_fer(name=None):
-    if name is None:
-        return 'One for you, one for me.'
-    else:
+def two_fer(name='you'):
         return 'One for {name}, one for me.'.format(name=name)
