e763444f787c7459e071a29ee71c8ccfa8afe47a
pmamueng <pmamueng@gmail.com>
2019-07-31 20:11:28 -0500

    Series module complete

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,14 @@
 def slices(series, length):
-    pass
+    final_list = []
+    index = 0
+
+    if len(series) < length or length < 1 or series == None:
+        raise ValueError("NO!")
+
+    while index <= len(series):
+        x = series[index:index+length]
+        if len(x) == length:
+            final_list.append(x)
+        index += 1
+
+    return final_list
